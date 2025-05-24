458b4d3ddff1c32cf8adc7d5529fce947139984b
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-24 11:29:01 +0530

    make some changes on admin page ✏️

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function AdminPage() {
     return(
-        <div className="w-full h-screen bg-red-900 flex">
-            <div className="h-full w-[300px] bg-blue-900">
-
+        <div className="w-full h-screen flex">
+            <div className="h-full w-[300px] flex flex-col">
+                <Link to="/admin/products">Products</Link>
+                <Link to="/admin/users">Users</Link>
+                <Link to="/admin/orders">Orders</Link>
+                <Link to="/admin/reviews">Reviews</Link>
             </div>
 
-            <div className="h-full w-[calc(100%-300px)] bg-yellow-900">
+            <div className="h-full w-[calc(100%-300px)]  bg-yellow-400">
 
             </div>
         </div>
