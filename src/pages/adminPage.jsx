88c6db0ae25b2b88628b3f3e74fef59c67ba2f5e
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-24 12:09:24 +0530

    new changes on admin page ✏️

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 
 export default function AdminPage() {
     return(
@@ -11,7 +11,12 @@ export default function AdminPage() {
             </div>
 
             <div className="h-full w-[calc(100%-300px)]  bg-yellow-400">
-
+                <Routes path="/*">
+                    <Route path="/products" element={<h1>Products</h1>} />
+                    <Route path="/users" element={<h1>Users</h1>} />
+                    <Route path="/orders" element={<h1>Orders</h1>} />
+                    <Route path="/reviews" element={<h1>Reviews</h1>} />
+                </Routes>
             </div>
         </div>
     )
