7ed2499ee6da82a6328b75508117e00e6c0543ab
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-26 09:02:26 +0530

    Create test page and use use useState hook ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import HomePage from './pages/home'
 import LoginPage from './pages/login'
 import SignUpPage from './pages/signup'
 import AdminPage from './pages/adminPage'
+import TestPage from './pages/testPage'
+
 
 function App() { //full app ------> App()
 
@@ -16,6 +18,7 @@ function App() { //full app ------> App()
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/testing" element={<TestPage />} />
           <Route path="/admin/*" element={<AdminPage />} />
           <Route path='/*' element={<h1>404 Not Found</h1>} />
         </Routes>
