467cad3e4ad6091f403965dc25f78efcfc84e10b
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-05-27 00:21:19 +0530

    updates on test page ✏️

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,7 @@
 export default function LoginPage(){
     return(
-        <div className="w-full min-h-screen bg-pink-100 flex flex-col items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-md flex flex-col items-center">
-        <h2 className="text-2xl font-bold text-blue-700 mb-4">Login</h2>
-        <input type="email" placeholder="Email" className="border p-2 mb-3 w-64 rounded" />
-        <input type="password" placeholder="Password" className="border p-2 mb-4 w-64 rounded" />
-        <button className="bg-blue-500 text-white px-8 py-2 rounded hover:bg-blue-600 font-semibold">Login</button>
+      <div className="w-full min-h-screen bg-pink-100 flex flex-col items-center justify-center">
+
       </div>
-    </div>
     )
 }
