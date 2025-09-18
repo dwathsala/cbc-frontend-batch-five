d70195d42550cc089f0017b50e04018e1aad89f5
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-09-18 22:44:05 +0530

    update sampe data on admin product page

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -20,27 +20,25 @@ export default function AdminProductsPage() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>COS006</td>
-                        <td>Hydrating Facial Cleanser</td>
-                        <td>
-                            <img src="https://example.com/images/cleanser1.jpg" className="w-[50px] h-[50px] object-cover" />
-                        </td>
-                        <td>$20.99</td>
-                        <td>$18.99</td>
-                        <td>50</td>
-                    </tr>
-
-                    <tr>
-                        <td>COS007</td>
-                        <td>Matte Liquid Lipstick</td>
-                        <td>
-                            <img src="https://example.com/images/lipstick1.jpg" className="w-[50px] h-[50px] object-cover" />
-                        </td>
-                        <td>$15.00</td>
-                        <td>$12.50</td>
-                        <td>120</td>
-                    </tr>
+                    {
+                        products.map(
+                            (item,index) =>{
+                                return(
+                                    <tr key={index}>
+                                        <td>{item.productId}</td>
+                                        <td>{item.name}</td>
+                                        <td>
+                                            <img 
+                                                src={item.images[0]} className="w-[50px] h-[50px]"/>
+                                        </td>
+                                        <td>${item.labelledPrice}</td>
+                                        <td>${item.price}</td>
+                                        <td>{item.stock}</td>
+                                    </tr>
+                                )
+                            }
+                        )
+                    }
                 </tbody>
 
             </table>
