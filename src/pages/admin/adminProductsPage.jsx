89f3fe7d3e3df2d88d1aed80f09f317887e59cff
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-09-18 08:39:58 +0530

    Register page creation

---
+++ src/pages/admin/adminProductsPage.jsx
@@ -0,0 +1,5 @@
+export default function AdminProductsPage() {
+    return(
+        <h1>Admin Products Page</h1>
+    )
+}
--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -12,7 +12,7 @@ export default function AdminPage() {
 
             <div className="h-full w-[calc(100%-300px)]  bg-yellow-400">
                 <Routes path="/*">
-                    <Route path="/products" element={<h1>Products</h1>} />
+                    <Route path="/products" element={<>Products</>} />
                     <Route path="/users" element={<h1>Users</h1>} />
                     <Route path="/orders" element={<h1>Orders</h1>} />
                     <Route path="/reviews" element={<h1>Reviews</h1>} />
