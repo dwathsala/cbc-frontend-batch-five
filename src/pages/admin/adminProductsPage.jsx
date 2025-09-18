8276a825960f6b07d6d8a1888b8b8a27093c95d8
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-09-18 09:49:26 +0530

    Admit peoduct page styling update✏️

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,5 +1,8 @@
 export default function AdminProductsPage() {
+    
     return(
-        <h1>Admin Products Page</h1>
+        <div className="w-full h-full bg-red-400 max-h-full overflow-y-scroll">
+            <div className="w-40px h-[1600px] border-[5px] border-blue-900"></div>
+        </div>
     )
 }
--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,4 +1,5 @@
 import { Link, Routes, Route } from "react-router-dom";
+import AdminProductsPage from "./admin/AdminProductsPage";
 
 export default function AdminPage() {
     return(
@@ -12,7 +13,7 @@ export default function AdminPage() {
 
             <div className="h-full w-[calc(100%-300px)]  bg-yellow-400">
                 <Routes path="/*">
-                    <Route path="/products" element={<>Products</>} />
+                    <Route path="/products" element={<AdminProductsPage/>} />
                     <Route path="/users" element={<h1>Users</h1>} />
                     <Route path="/orders" element={<h1>Orders</h1>} />
                     <Route path="/reviews" element={<h1>Reviews</h1>} />
