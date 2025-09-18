42e610aa0fd17d3fdb3626607693bfa0c2f1a6a2
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-09-18 11:22:35 +0530

    update sample data of admin page✏️

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,11 +1,23 @@
-import { sampleProucts } from "../../assets/sampleData";
+import { useState } from "react";
+import { sampleProducts } from "../../assets/sampleData";
 
 export default function AdminProductsPage() {
-    const [products, setProducts] = useState(sampleProucts); 
+
+    const [products, setProducts] = useState(sampleProducts)
 
     return(
-        <div className="w-full h-full bg-red-400 max-h-full overflow-y-scroll">
-            <div className="w-40px h-[1600px] border-[5px] border-blue-900"></div>
+        <div className="w-full h-full max-h-full overflow-y-scroll">
+            <table className="w-full">
+                <thead>
+                    <tr>
+                        <th>Product ID</th>
+                        <th>Name</th>
+                        <th>Image</th>
+                        <th>Labelled Price</th>
+                        <th>Stock</th> 
+                    </tr> 
+                </thead>
+            </table>
         </div>
     )
 }
