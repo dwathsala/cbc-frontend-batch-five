692359e1186dc5b73f3b7211641f972db2a2a279
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-09-18 10:28:36 +0530

    Added sample data to admin product page ✏️

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,5 +1,8 @@
+import { sampleProucts } from "../../assets/sampleData";
+
 export default function AdminProductsPage() {
-    
+    const [products, setProducts] = useState(sampleProucts); 
+
     return(
         <div className="w-full h-full bg-red-400 max-h-full overflow-y-scroll">
             <div className="w-40px h-[1600px] border-[5px] border-blue-900"></div>
