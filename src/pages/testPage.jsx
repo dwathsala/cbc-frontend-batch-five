ba91687549c40a46dd63d03edd82efdaa2435ca2
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-09-22 18:38:08 +0530

    Testing⚙️

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -2,61 +2,22 @@ import { useState } from "react";
 import { compile } from "tailwindcss";
 
 export default function TestPage() {
+    const [image, setImage] = useState(null);
 
-    const [count,setCount] = useState(0)  //useState(0) initializes count is 0   //setCount is use to change the value of count and rerun the component in function without hooks
-    const [status,setStatus] = useState("Passed") //useState("Passed") initializes status is Passed
+    function fileUpload(){
+        console.log("button clicked");
+    }
 
     return(
         <div className="w-full h-screen flex justify-center items-center flex-col">
-            <div className="w-[450px] h-[250px] shadow flex justify-center items-center">
-                <button onClick={()=>{
-                    //console.log("- clicked");
-                    setCount(count - 1);
+            <input type="file" className="file-input file-input-bordered w-full max-w-xs" 
+            onChange={(e)=>{
+                
+                setImage(e.target.files[0]);
 
-                }} className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer">
-                    -
-                </button>
-
-                <span className="text-[40px] font-bold text-center w-[100px] h-[40px] mx-[10px] flex justify-center items-center">
-                    {count}
-                </span>
-
-                 <button onClick={()=>{
-                    //console.log("+ clicked");
-                    setCount(count + 1);
-
-                }} className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer">
-                    +
-                </button>
-            </div>
-
-            
-
-            <div className="w-[450px] h-[250px] shadow flex flex-col justify-center items-center">
-                {/*id user passed or failed span tag*/}
-                <span className="text-[40px] font-bold text-center w-[100px] h-[40px] mx-[10px] flex justify-center items-center">
-                    {status}
-                </span>
-
-                <div>
-                    <button className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer m-[20px]" onClick={
-                        ()=>{
-                            setStatus("Passed");
-                            }
-                    }>
-                        Passed
-                    </button>
-
-                    <button className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer m-[20px]" onClick={
-                        ()=>{
-                            setStatus("Failed");
-                            }
-                    }>
-                        Failed
-                    </button>
-                </div>
-            </div>
-
+            }}/>
+            <button onClick={fileUpload}                                 className="bg-green-500 text-white font-bold py-2 px-4 rounded">Upload</button>
         </div>
+            
     )
 }
