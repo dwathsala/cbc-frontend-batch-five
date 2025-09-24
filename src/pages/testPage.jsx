ae1123921e9c21b86dca685a3b7180838ef0e3cc
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-09-24 18:05:14 +0530

    mediaUpload.jsx file update✏️

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -6,22 +6,11 @@ import { compile } from "tailwindcss";
 export default function TestPage() {
     const [image, setImage] = useState(null);
 
-    const url = "https://zufnjkshbpewbmbsbviy.supabase.co";
-    const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1Zm5qa3NoYnBld2JtYnNidml5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1MjgzMTMsImV4cCI6MjA3NDEwNDMxM30.RAmYTgvIB4RTRtA8vsauO8M2jzm0_r9jl-mv-cEXxVg";
-
+    
     const supabase = createClient(url, key); //make connection with supabase
 
     function fileUpload(){
-        supabase.storage.from("images").upload(image.name, image, {
-            upsert: false,
-            cacheControl: '3600',
-        }).then((res)=>{
-            const publicUrl = supabase.storage.from("images").getPublicUrl(image.name).data.publicUrl
-            console.log(publicUrl);
         
-        }).catch((e)=>{
-            console.log(e);
-        })
     }
 
     return(
