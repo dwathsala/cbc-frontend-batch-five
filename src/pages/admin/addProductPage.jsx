0bfb82e409581edc48229257eb5757b49e1c4c01
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-09-25 14:12:26 +0530

    update add product page✏️

--- src/pages/admin/addProductPage.jsx
+++ src/pages/admin/addProductPage.jsx
@@ -1,7 +1,5 @@
-import { href } from "react-router-dom";
-
 export default function AddProductPage() {
     return(
-    <div className="w-full h-full flex flex-col items-center justify-center bg-green-500"></div>
+    <div className="w-full h-full flex flex-col justify-center items-center bg-green-400"></div>
 )
 }
