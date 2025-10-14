a6fac091204f73a314f4e2517fbdb0a50e26bc67
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-10-14 17:16:30 +0530

    make load to product details into prduct overview page ✏️

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -1,9 +1,36 @@
+import axios from "axios";
+import { useState , useEffect } from "react";
+import toast from "react-hot-toast";
 import { useParams } from "react-router-dom"
 
 export default function ProductOverview() {
     const params = useParams();
     const productId = params.id;
+    const [status, setStatus] = useState("loading"); //loading, error, success
+    const [product, setProduct] = useState(null);
+
+    useEffect(
+        ()=>{
+            axios.get(import.meta.env.VITE_BACKEND_URL+"/api/products/"+productId).then(
+                (res)=>{
+                    console.log(res.data);
+                    setProduct(res.data);
+                    setStatus("success");
+
+                }
+            ).catch(
+                (err)=>{
+                    console.log(err);
+                    setStatus("error");
+                    toast.error("Error fetching product details");
+                }
+            )
+        },[]
+    )
+
     return (
-        <div>This is overview page for product {productId}</div>
+        <div>
+            This is overview page for product {productId} {JSON.stringify(product)}
+        </div>
     )
 }
