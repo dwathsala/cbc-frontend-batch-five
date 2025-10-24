11a633fb9bdd3e57d6cf8d0fd6f89dee7b11e519
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-10-24 23:23:32 +0530

    modify the image slider 🔗

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -3,6 +3,7 @@ import { useState , useEffect } from "react";
 import toast from "react-hot-toast";
 import { useParams } from "react-router-dom"
 import Loading from "../../components/loading";
+import ImageSlider from "../../components/imageSlider";
 
 export default function ProductOverview() {
     const params = useParams();
@@ -34,12 +35,12 @@ export default function ProductOverview() {
         {
         status == "success" && ( 
         <div className="w-full h-full flex flex-row"> 
-            <div className="w-[50%] h-full">
-                <imageSlider images={product.images} />
+            <div className="w-[50%] h-full flex justify-center items-center">
+                <ImageSlider images={product.images} />
             </div>  
 
             <div className="w-[50%] h-full bg-blue-500">
-            
+                
             </div>          
         </div>
         )}
