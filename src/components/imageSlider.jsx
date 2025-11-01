edfe5b7d7bcf337debbd0aff6adfd5db642bd5ca
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-01 09:28:56 +0530

    modify images in image slider✏️

--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -1,15 +1,17 @@
-export default function ImageSlider(props) {
+import { useState } from "react";
 
+export default function ImageSlider(props) {
     const images = props.images;
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     return (
         <div className="w-[500px] h-[600px] bg-red-900">
-            <img className="w-full h-[500px]" src="" alt="" />
+            <img src={images[currentIndex]} className="w-full h-[500px] object-cover" /> 
             <div className="w-full h-[100px] flex justify-center items-center">
                 {images?.map( //? if there is images then only map
                     (image,index) => {
                         return (    
-                            <img className="w-[80px] h-[80px] m-2 rounded-2xl cursor-pointer" src={image} />
+                            <img className={"w-[80px] h-[80px] m-2 rounded-2xl cursor-pointer object-cover" }src={image} />
                         )
                     }
                 )}
