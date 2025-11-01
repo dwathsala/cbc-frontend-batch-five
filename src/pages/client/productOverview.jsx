019a238baca62890dc0c686679a800edff5879b2
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-01 19:48:24 +0530

    product description added on product overview page✏️

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -39,9 +39,39 @@ export default function ProductOverview() {
                 <ImageSlider images={product.images} />
             </div>  
 
-            <div className="w-[50%] h-full flex flex-col">
-                <h1 className=""></h1>
-                
+            <div className="w-[50%] h-full flex justify-center items-center">
+                <div className="w-[500px] h-[600px] flex flex-col items-center">
+                    <h1 className="w-full text-center text-4xl font-semibold text-secondary-text">{product.name}
+                        {
+                            product.altName.map((altName,index)=>{
+                                return (
+                                    <span key={index} className="text-2xl font-normal text-gray-500"> {" | " +altName} </span>
+                                )
+                            })
+                        }
+                    </h1>
+
+                    <h1 className="w-full text-center my-2 text-md text-gray-600 font-semibold">{product.productId}</h1>
+                    <p className="w-full text-center my-2 text-md text-gray-600 font-semibold">{product.description}</p>
+                    {
+                        product.labelledPrice > product.price?
+                        <div>
+                            <span className="text-4xl mx-4 text-gray-500 line-through">{product.labelledPrice.toFixed(2)}</span>
+                            <span className="text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
+                        </div>
+                        :
+                        <div>
+                            <span className="text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
+                        </div>
+                    }
+
+                    <div className="w-full flex justify-center items-center">
+                        <button className="bg-green-700 text-white px-6 py-3 rounded-full mt-6 hover:bg-accent-dark mx-4 font-bold cursor-pointer hover:bg-green-800 active:bg-green-600">Add to Cart</button>
+                        <button className="bg-green-700 text-white px-6 py-3 rounded-full mt-6 hover:bg-accent-dark mx-4 font-bold cursor-pointer  hover:bg-green-800 active:bg-green-600">Buy Now</button>
+
+                    </div>
+
+                </div>
             </div>          
         </div>
         )}
