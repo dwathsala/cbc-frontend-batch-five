e4b0a78124da2ff0a944c7cce87c0462f5eb4218
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-20 00:17:12 +0530

    create adminOrderPage✏️

---
+++ src/pages/admin/adminOrdersPage.jsx
@@ -0,0 +1,8 @@
+import
+
+export default function AdminOrdersPage() {
+
+    const [orders, setOrders] = useState([]);
+    const [status, setStatus] = useState("");
+    const [isLoading, setIsLoading] = useState(true);    
+}
