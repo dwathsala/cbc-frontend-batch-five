3a2d5c580518a5424e8765f66d395c0e59f9c2cf
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-20 06:59:53 +0530

    make some updates on adminOrderPage

--- src/pages/admin/adminOrdersPage.jsx
+++ src/pages/admin/adminOrdersPage.jsx
@@ -1,8 +1,39 @@
-import
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function AdminOrdersPage() {
 
     const [orders, setOrders] = useState([]);
     const [status, setStatus] = useState("");
-    const [isLoading, setIsLoading] = useState(true);    
+    const [isLoading, setIsLoading] = useState(true);   
+    
+    useEffect(() => {
+        if (isLoading) { 
+            const Token = localStorage.getItem("Token");
+            if (!Token) {
+                alert("Please login first.");
+                return;
+            }
+            axios.get(import.meta.env.VITE_API_URL + "/api/orders", {
+                headers: {
+                    Authorization: "Bearer " + Token,
+                },
+            }).then((res) => {
+                setOrders(res.data);
+                setIsLoading(false);
+            }
+            ).catch((err) => {
+                alert("Error fetching orders: " + err.response?.data?.message || "unknown error");
+                setIsLoading(false);
+            });
+        }
+
+          
+    }, [isLoading]);
+               
+    return (
+        <div>
+            
+        </div>
+    );
 }
