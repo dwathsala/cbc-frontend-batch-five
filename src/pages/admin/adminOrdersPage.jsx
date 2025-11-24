3b679b98242e54a39f7b46fa22a7f4a50197be34
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-24 16:42:26 +0530

    solve syntax error in adminOrderPage🔗

--- src/pages/admin/adminOrdersPage.jsx
+++ src/pages/admin/adminOrdersPage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Loading from "../../components/Loading";
+import Loading from "../../components/loading";
 import Modal from "react-modal";
 
 Modal.setAppElement("#root");
