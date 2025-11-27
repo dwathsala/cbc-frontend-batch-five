59e86538b61d63de9dd909ad3faed32c60c46a8a
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-27 13:06:35 +0530

    update🔗

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,32 +1,8 @@
-import { useState } from "react";
-import mediaUpload from "../utils/mediaUpload";
-
 export default function TestPage() {
-  const [image, setImage] = useState(null);
-
-  function fileUpload() {
-    mediaUpload(image)
-      .then((res) => {
-        console.log("Uploaded URL:", res);
-      })
-      .catch((err) => {
-        console.log("Error:", err);
-      });
-  } 
 
   return (
     <div className="w-full h-screen flex justify-center items-center flex-col">
-      <input
-        type="file"
-        className="file-input file-input-bordered w-full max-w-xs"
-        onChange={(e) => setImage(e.target.files[0])}
-      />
-      <button
-        onClick={fileUpload}
-        className="bg-green-500 text-white font-bold py-2 px-4 rounded mt-4"
-      >
-        Upload
-      </button>
+      
     </div>
   );
 } 
