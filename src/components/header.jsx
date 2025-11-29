c639d3fc8504fbf3d156e3495cc72a1a57861680
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-29 19:25:48 +0530

    updae header component for mobile responsive✏️

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,16 +2,19 @@ import UserData from "./userData";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { BsCart3 } from "react-icons/bs";
+import { FaHamburger } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Header(){
     const navigate = useNavigate();
     console.log("Header component loading....")
     return(
         <header className="w-full h-[80px] shadow-2xl flex">
+            <GiHamburgerMenu/>
             <img onClick={()=>{
                 navigate("/")
             }} src="/logo.png" alt="Logo" className="w-[80px] h-[80px] object-cover cursor-pointer" />
-            <div className="w-[calc(100%-16px)] h-full flex justify-center items-center" >
+            <div className="w-[calc(100%-16px)] h-full hidden md:flex justify-center items-center" >
                 <Link to="/" className="text-[20px] font-bold mx-2">Home</Link>
                 <Link to="/products" className="text-[20px] font-bold mx-2">Products</Link>
                 <Link to="/about" className="text-[20px] font-bold mx-2">About</Link>
