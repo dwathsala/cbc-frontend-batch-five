c87ad2aadc700dd9066324aab0ce7f2724084bc3
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-29 11:45:39 +0530

    Test mobile responsive in testPage✏️

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,7 +1,7 @@
 export default function TestPage() {
 
   return (
-    <div className="w-full h-screen flex justify-center items-center flex-col">
+    <div className="w-full h-screen flex justify-center items-center flex-col bg-emerald-700 md:bg-blue-400 lg:bg-red-300 xl:bg-amber-600">
       
     </div>
   );
