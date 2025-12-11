77e6bd676d39bba67d7919bde20d368e3b73b615
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-12-12 00:55:30 +0530

    Update google login⚙️

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,3 +1,4 @@
+import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
@@ -9,6 +10,17 @@ export default function LoginPage(){
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate =  useNavigate()
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: (response) => {
+      const accessToken = response.access_token;
+      axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login/google", {
+        accessToken: accessToken
+      }).then((response)=>{
+        console.log(response.data);
+      })
+    }
+  })
 
   async function handleLogin() { // http request going to backend with email and password
     //console.log(email)
@@ -79,7 +91,7 @@ export default function LoginPage(){
                 Login
             </button>
 
-            <button className="flex items-center justify-center gap-3 w-[300px] h-[50px] px-6 bg-white border-2 border-gray-300 rounded-[20px] hover:bg-[#00000060] hover:border-[#00000060] active:bg-[#00000030] active:scale-95 transition-all duration-200 shadow-sm hover:shadow-md font-bold text-gray-700 hover:text-white cursor-pointer">
+            <button onClick={googleLogin} className="flex items-center justify-center gap-3 w-[300px] h-[50px] px-6 bg-white border-2 border-gray-300 rounded-[20px] hover:bg-[#00000060] hover:border-[#00000060] active:bg-[#00000030] active:scale-95 transition-all duration-200 shadow-sm hover:shadow-md font-bold text-gray-700 hover:text-white cursor-pointer">
               <svg className="w-5 h-5" viewBox="0 0 24 24">
                 <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
                 <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
