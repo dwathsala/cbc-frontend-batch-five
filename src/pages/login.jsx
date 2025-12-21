8654858411bd0d4961fd96eb089d70d1fe7a8bc8
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-12-21 13:09:26 +0530

    solve issue in click login button ⚙️

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -9,6 +9,7 @@ export default function LoginPage(){
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate =  useNavigate()
 
   const googleLogin = useGoogleLogin({
