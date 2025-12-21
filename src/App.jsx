634cb3ae36f818011bb769c495e38995e1f240a9
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-12-21 13:11:41 +0530

    import Navigate from react-router-dom⚙️

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes,Navigate } from 'react-router-dom'
 import './App.css'
 import Header from './components/header'
 import ProductCard from './components/productCard'
